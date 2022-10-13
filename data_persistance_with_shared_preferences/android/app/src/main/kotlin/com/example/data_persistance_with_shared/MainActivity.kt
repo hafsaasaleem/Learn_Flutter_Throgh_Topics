5027651f3bcd0745f6d5f5390207bc733729a805
hafsaasaleem <shaikhhafsa215@gmail.com>
2022-10-13 12:38:00 +0500

    first commit

---
+++ data_persistance_with_shared_preferences/android/app/src/main/kotlin/com/example/data_persistance_with_shared/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.data_persistance_with_shared
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ data_persistance_with_shared_preferences/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String name = '';
+  late TextEditingController controller;
+  late SharedPreferences preferences;
+
+  Future init() async {
+    preferences = await SharedPreferences.getInstance();
+    final String? name = preferences.getString('name');
+    if (name != null) setState(() => this.name = name);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+    init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Shared Preferences")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(name),
+            const SizedBox(height: 30),
+            TextField(
+              controller: controller,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () async {
+                preferences.setString('name', controller.text);
+                final String? name = preferences.getString('name');
+                if (name != null) setState(() => this.name = name);
+                controller.clear();
+              },
+              child: const Text("Change"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+  }
+}
+
+
